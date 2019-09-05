306e988c07892d0be7191f6ae42982810eea6e5e
skuznet <azon.sk@gmail.com>
2019-09-05 22:16:47 +0300

    add basic proj structure

--- src/main/java/com/learn/controller/AppController.java
+++ src/main/java/com/learn/controller/AppController.java
@@ -1,4 +1,15 @@
 package com.learn.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AppController {
+    // localhost:8080/
+    @RequestMapping("/")
+    public String getHelloPage(Model model) {
+        model.addAttribute("name", "world!");
+        return "hello";
+    }
 }
